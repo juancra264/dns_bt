a404fefc38dbf3e5037d795883ade5041e25798f
juancra264 <juancra264@hotmail.com>
2018-08-10 10:25:40 -0500

    Mejora de script de adicion de zona DNS

--- add-domain.sh
+++ add-domain.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+## 
+# Script para creacion de zona DNS BT LATAM COLOMBIA.
+#
+##
+
+# Variables Globales
 NAMED_CONF_PATH="/etc/bind/named.conf.bt.zones"
 BIND_DB_PATH="/etc/bind/master"
 SERIAL_NO_COUNT=`date +%Y%m%d01`
@@ -5,49 +12,54 @@ NS1="ns1.btlatam.com.co"
 NS2="ns2.btlatam.com.co"
 NS3="ns3.btlatam.com.co"
 NS4="ns4.btlatam.com.co"
-ADMIN_EMAIL="dc\.ar.bt.com"
+ADMIN_EMAIL="dns-adm.btlatam.com.co"
 ZONE_DB_FILE=""
 domainname=""
 ipmainadd=""
 ipwebadd=""
 serial_no=1
+
+# Ingreso de ZONA a crear
 echo -n "Enter domain name : "
 read domainname
 ZONE_DB_FILE="$BIND_DB_PATH/db.$domainname"
-echo -n "Enter IP Address for main domain record : "
-read ipmainadd
-echo -n "Enter IP Address for www  record : "
-read ipwebadd
 
+#echo -n "Enter IP Address for main domain record : "
+#read ipmainadd
+#echo -n "Enter IP Address for www  record : "
+#read ipwebadd
+
+# Revisa que no exista la zona DNS en la configuracion.
 if grep \"$domainname\" $NAMED_CONF_PATH > /dev/null ; then
 	echo "Domain $domainname is already present, please check it up"
 	exit 1
 fi
 
-if which ipcalc > /dev/null ; then
+# Revisa la informacion de IP ingresas para los registros
+#if which ipcalc > /dev/null ; then
+#
+#	ipcalc -ms $ipmainadd > /dev/null
+#	if ! [ $? -eq 0 ] ; then
+#		echo "Ip Main Address is wrong, please try again"
+#		exit 2
+#	fi
+#
+#
+#	ipcalc -ms $ipwebadd > /dev/null
+#	if ! [ $? -eq 0 ] ; then
+#		echo "Ip WEB Address is wrong, please try again"
+#		exit 2
+#	fi
+#fi
 
-	ipcalc -ms $ipmainadd > /dev/null
-	if ! [ $? -eq 0 ] ; then
-		echo "Ip Main Address is wrong, please try again"
-		exit 2
-	fi
-
-
-	ipcalc -ms $ipwebadd > /dev/null
-	if ! [ $? -eq 0 ] ; then
-		echo "Ip WEB Address is wrong, please try again"
-		exit 2
-	fi
-fi
-
-
+# Adiciona al final en el archivo de Zonas el nuevo Dominio
 echo "zone 	\"$domainname\" {" >> $NAMED_CONF_PATH
 echo "		type master;" >> $NAMED_CONF_PATH
 echo "		file \"$ZONE_DB_FILE\";" >> $NAMED_CONF_PATH
 echo "};" >> $NAMED_CONF_PATH
 echo " " >> $NAMED_CONF_PATH
 
-
+# Crea archivo de configuracion ZONA DNS del dominio 
 echo '$ORIGIN' $domainname. >> $ZONE_DB_FILE
 echo '$TTL 43200' >> $ZONE_DB_FILE
 echo "$domainname.	IN	SOA	$NS1.	$ADMIN_EMAIL.	(" >> $ZONE_DB_FILE
@@ -57,14 +69,14 @@ echo "			3600	; retry" >> $ZONE_DB_FILE
 echo "			604800	; expire" >> $ZONE_DB_FILE
 echo "			38400	; TTL" >> $ZONE_DB_FILE
 echo "			)"	>> $ZONE_DB_FILE
-echo "@				IN	NS	$NS1." >> $ZONE_DB_FILE
-echo "@				IN	NS	$NS2." >> $ZONE_DB_FILE
-echo "@				IN	NS	$NS3." >> $ZONE_DB_FILE
-echo "@				IN	NS	$NS4." >> $ZONE_DB_FILE
-echo "@				IN	A	$ipmainadd" >> $ZONE_DB_FILE
-echo "www				IN	A	$ipwebadd" >> $ZONE_DB_FILE
+echo "@			IN	NS	$NS1." >> $ZONE_DB_FILE
+echo "@			IN	NS	$NS2." >> $ZONE_DB_FILE
+echo "@			IN	NS	$NS3." >> $ZONE_DB_FILE
+echo "@			IN	NS	$NS4." >> $ZONE_DB_FILE
 
 echo "$domainname ($ipmainadd) Added succesfully."
-/etc/init.d/bind9 restart
-named-checkzone $domainname $BIND_DB_PATH/db.$domainname
 
+named-checkzone $domainname $BIND_DB_PATH/db.$domainname
+named-checkconf	/etc/bind/named.conf
+echo "Please sync the DNS servers:"
+echo " execute sh /root/sync-zones.sh"
